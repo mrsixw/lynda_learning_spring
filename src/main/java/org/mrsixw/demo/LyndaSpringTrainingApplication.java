bad29bee54ad03d2b624606b562c4b573eb7b308
Steve Williams <mrsixw@gmail.com>
2018-04-22 11:05:35 +0100

    Default project init

---
+++ src/main/java/org/mrsixw/demo/LyndaSpringTrainingApplication.java
@@ -0,0 +1,12 @@
+package org.mrsixw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyndaSpringTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyndaSpringTrainingApplication.class, args);
+	}
+}
